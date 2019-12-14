120d9b31310ae328b468e5607d1cf38e82c558d4
ArnauAzorin <arnau.azorin@gmail.com>
2019-12-14 02:51:32 +0100

    BlackRat

---
+++ Black Rat 2.1/Assets/Scripts/DestroyBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DestroyBox : MonoBehaviour
+{
+    bool box = false;
+    bool enemy = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Caja")
+        {
+            box = true;
+            Debug.Log("box true.");
+        }
+        if (other.tag == "Enemy")
+        {
+            enemy = true;
+            Debug.Log("enemy true.");
+        }
+        if (other.tag == "Win")
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.A) && box == true)
+        {
+            Destroy(GameObject.FindWithTag("Caja"));
+            Debug.Log("A key was pressed.");
+            box = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A) && enemy == true)
+        {
+            Destroy(GameObject.FindWithTag("Enemy"));
+            Debug.Log("A key was pressed.");
+
+        }
+    }
+}
---
+++ Black Rat 2.1/Assets/Scripts/LogoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoManager : MonoBehaviour
+{
+    public float delay = 440;
+    void Start()
+    {
+       
+        StartCoroutine(LoadMenu(delay));
+    }
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+    IEnumerator LoadMenu(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("TitleScene");
+    }
+}
